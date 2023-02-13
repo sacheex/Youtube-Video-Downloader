ef286d4c180a35e8b9eaa6e3c463adf0b62d65c5
Axob12 <sachithdhanushka00@gmail.com>
2023-02-13 18:31:29 +0530

    Update

--- youtubeDwnldr.py
+++ youtubeDwnldr.py
@@ -22,14 +22,43 @@ class Downloader:
         self.link = ctk.CTkEntry(self.app, width=350, height=40, textvariable=self.url_var)
         self.link.pack()
 
-        self.download = ctk.CTkButton
+        self.finishLabel = ctk.CTkLabel(self.app, text="")
+        self.finishLabel.pack()
 
+        # Download percentage
+        self.percentage = ctk.CTkLabel(self.app, text="0%")
+        self.percentage.pack()
 
+        # Download progress bar
+        self.progressBar = ctk.CTkProgressBar(self.app, width=400)
+        self.progressBar.set(0)
+        self.progressBar.pack()
 
-
-
-
+        self.download = ctk.CTkButton(self.app, text="Download", command=self.startDownload)
+        self.download.pack(padx=10, pady=10)
 
         self.app.mainloop()
+
+    def startDownload(self):
+        try:
+            ytLink = self.link.get()
+            ytObject = YouTube(ytLink, on_progress_callback=self.onProgress)
+            video = ytObject.streams.get_lowest_resolution()
+            self.title.configure(text=ytObject.title, text_color="white")
+            self.finishLabel.configure(text="")
+            video.download()
+            self.finishLabel.configure(text=" Downloded!")
+        except:
+            self.finishLabel.configure(text="Download Error", text_color="red")
+
+    def onProgress(self, stream, chunk, bytes_remaining):
+        totalSize = stream.filesize
+        bytes_downloded = totalSize - bytes_remaining
+        percentageOfCompletion = bytes_downloded/totalSize * 100
+        per = str(int(percentageOfCompletion))
+        self.percentage.configure(text = per + "%")
+        self.percentage.update()
+
+        self.progressBar.set(float(percentageOfCompletion)/100)
 
 Downloader()
